29a113b3013bf1a0f997978c985f9d3544df0384
chester <lsortnj@gmail.com>
2016-12-15 00:13:14 +0800

    fixed bottom and bootstrap

--- content_script.js
+++ content_script.js
@@ -1,5 +1,13 @@
 $(document).ready(function() { 
   var link1=chrome.extension.getURL("icon.png");
-  var mo = "<div id='infoPanel'><h1>YOYO MO</h1></div>";
-  $("body").append(mo);
+  var infoPanel = $("<div>").attr('id', 'infoPanel').attr('class', 'row');
+  var title = $("<h4>").html("留言討論區");
+  var comments  = $("<div>").attr('class', 'comments col-md-8 col-xs-12').html("Comments");
+  var userBlock = $("<div>").attr('class', 'user-info col-md-4 col-xs-12');
+  var btnLogin  = $("<button>").attr('class', 'btn btn-success').text("立即登入留言");
+  
+  userBlock.append(btnLogin);
+  infoPanel.append(title).append(comments).append(userBlock);
+  
+  $("body").append(infoPanel);
 });
